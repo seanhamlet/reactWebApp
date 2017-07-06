56bcc89145374130605548e4cbc6b897ba64dc23
Sean M Hamlet <seanmhamlet@gmail.com>
2017-07-06 09:21:01 -0400

    Creates simple react app with JSX

---
+++ 03-HelloReact/public/app.jsx
@@ -0,0 +1,7 @@
+// Using html and JSX (JavaScript XML) in this example
+// not making a React component
+// h1 tag is what is 'rendered' and 'location' to be rendered is next parameter
+ReactDOM.render(
+  <h1>Hello React!</h1>,
+  document.getElementById('app')
+);
