58934411800ae7c06cbd2ccef81d19ffb2d241c9
Sean M Hamlet <seanmhamlet@gmail.com>
2017-07-06 16:18:14 -0400

    Adds form and button to capture user input

--- 03-HelloReact/public/app.jsx
+++ 03-HelloReact/public/app.jsx
@@ -7,6 +7,18 @@ var Greeter = React.createClass({
       message: 'This is from a component!'
     };
   },
+  onButtonClick: function (e) {
+    // prevents form from submitting and causing a page refresh
+    // since we are working with React we want to use single page application ideas
+    // one of those ideas is to NOT have browser completely refresh the app
+    e.preventDefault();
+
+    // this.refs contains the "ref" name defined in render in the input element
+    // need to get "value" from input called "name" to get it's textbox value
+    var name = this.refs.name.value;
+
+    alert(name);
+  },
   render: function () {
     // get props for component
     var name = this.props.name;
@@ -16,6 +28,12 @@ var Greeter = React.createClass({
       <div>
         <h1>Hello {name}!</h1>
         <p>{message + '!!'}</p>
+        {/* ref is a React attribute that allows us to give an 
+        element name we want to access later*/}
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
