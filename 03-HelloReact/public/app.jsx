2cf0bc74d281d2d27eab65ca1861f53055e8763b
Sean M Hamlet <seanmhamlet@gmail.com>
2017-07-06 17:04:08 -0400

    Adds state to component to update h1 tag

--- 03-HelloReact/public/app.jsx
+++ 03-HelloReact/public/app.jsx
@@ -7,21 +7,35 @@ var Greeter = React.createClass({
       message: 'This is from a component!'
     };
   },
+  getInitialState: function () {
+    return {
+      name: this.props.name
+    };
+  },
   onButtonClick: function (e) {
     // prevents form from submitting and causing a page refresh
     // since we are working with React we want to use single page application ideas
     // one of those ideas is to NOT have browser completely refresh the app
     e.preventDefault();
 
+    var nameRef = this.refs.name;
+
     // this.refs contains the "ref" name defined in render in the input element
     // need to get "value" from input called "name" to get it's textbox value
-    var name = this.refs.name.value;
+    var name = nameRef.value;
+    // reset input to nothing on submit click
+    nameRef.value = '';
 
-    alert(name);
+    // make sure empty string doesn't update state
+    if (typeof name === 'string' & name.length > 0) {
+      this.setState({
+        name: name
+      });
+    }
   },
   render: function () {
     // get props for component
-    var name = this.props.name;
+    var name = this.state.name;
     var message = this.props.message;
 
     return (
