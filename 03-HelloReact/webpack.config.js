a29ccc36ecba2209c0363a8ef540fe7296ce8ff8
Sean M Hamlet <seanmhamlet@gmail.com>
2017-07-11 11:53:52 -0400

    Updates webpack to find and parse jsx files

--- 03-HelloReact/webpack.config.js
+++ 03-HelloReact/webpack.config.js
@@ -1,10 +1,22 @@
 module.exports = {
-  entry: './public/app.js', // where webpack to start processing code
+  entry: './public/app.jsx', // where webpack to start processing code
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
   resolve: {
     extensions: ['', '.js', '.jsx'] // list of file extensions we want to process
+  },
+  module: {
+    loaders: [ // list of loaders, each loader is within an object
+      {
+        loader: 'babel-loader', // tell babel-loader to get files, and parse them with react and es2015
+        query: { // tell loader what to do
+          presets: ['react', 'es2015']
+        },
+        test: /\.jsx?$/, // which files to get (in this case, all *.jsx files)
+        exclude: /(node_modules|bower_components)/ // folders to exclude
+      }
+    ]
   }
 };
