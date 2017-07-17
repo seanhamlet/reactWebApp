e8ada44834c5cb1d8cbed8a279dedf3a0c2282c0
Sean M Hamlet <seanmhamlet@gmail.com>
2017-07-17 14:55:56 -0400

    Refactor code using arrow functions, es6 destructuring, and statemess functional components

--- 05-ReactWeather/app/components/Main.jsx
+++ 05-ReactWeather/app/components/Main.jsx
@@ -1,18 +1,16 @@
 var React = require('react');
 var Nav = require('Nav');
 
-var Main = React.createClass({
-  render: function () {
-    return (
+var Main = (props) => {
+  return (
       // remember you can only return one "root" element (in this case, a 'div')
       <div>
         <Nav/>
         <h2>Main Component</h2>
-        {this.props.children}
+        {props.children}
       </div>
       // adding 'this.props.children' tells Main where to place children components
-    );
-  }
-});
+  );
+};
 
 module.exports = Main;
--- 05-ReactWeather/app/components/WeatherMessage.jsx
+++ 05-ReactWeather/app/components/WeatherMessage.jsx
@@ -1,12 +1,9 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-  render: function () {
-    var {temp, location} = this.props;
-    return (
+var WeatherMessage = ({temp, location}) => {
+  return (
       <h3>It's {temp} in {location}.</h3>
-    )
-  }
-});
+  )
+};
 
 module.exports = WeatherMessage;
